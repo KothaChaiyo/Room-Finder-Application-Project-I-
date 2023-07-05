253e3c9c4953a4e3fcaf20c5d26d85758085c921
Abhinab <abhinabkhanal@live.com>
2023-07-05 18:16:21 +0545

    Implemented turkish button validation logic remains this commit is to update my progress

--- script/turkishButton.js
+++ script/turkishButton.js
@@ -4,36 +4,40 @@
 const landLord = document.getElementById('landLord');
 const tenant = document.getElementById('tenant');
 
-landLord.addEventListener('click', () => {
+
+
+
+
+
+//getting form elements
+const password = document.getElementById('password')
+const rePassword = document.getElementById('rePassword')
+const fName = document.getElementById("fName")
+const lName = document.getElementById("lName")
+const contact = document.getElementById("contact")
+const email = document.getElementById("email")
+
+
+
+
+
+const OFFSET = 25;
+
+landLord.addEventListener('focus', () => {
     alert("Form not filled Correctly")
 })
 
-tenant.addEventListener('click', () => {
+tenant.addEventListener('focus', () => {
     alert("Form not filled Correctly")
 })
+
+
 
 
 document.addEventListener('mousemove',(e) => {
-    const x = e.pageX
-    const y = e.pageY
-
-
-    const landLordButtonBox = landLord.getBoundingClientRect();
-    const tenantButtonBox = tenant.getBoundingClientRect();
-
-    const horizontalDistanceFromLandLord = (landLordButtonBox.x, x , landLordButtonBox.width);
-    const horizontalDistanceFromTenant = (tenantButtonBox.x, x , tenantButtonBox.width);
-
-    const verticalDistanceFromLandLord = (landLordButtonBox.y, y , landLordButtonBox.height);
-    const verticalDistanceFromTenant = (tenantButtonBox.y, y , tenantButtonBox.height);
-
-
-    if(Math.abs(horizontalDistanceFromLandLord) <= 100 && Math.abs(verticalDistanceFromLandLord) <=100 ){
-        changePosition();
-    }else {
-        resetPosition();
-    }
-
+   
+    
+    turkishButton(e);
 
 })
 
@@ -41,10 +45,71 @@ function distanceFromCenter(boxPosition, mousePosition, boxSize){
     return boxPosition - mousePosition + boxSize / 2
 }
 
-function changePosition(){
-    console.log(verticalDistanceFromLandLord);
-}
 
 function resetPosition(){
     console.log('reset')
+}
+
+
+function turkishButton(e){
+    const x = e.pageX
+    const y = e.pageY
+
+    
+    const landLordButtonBox = landLord.getBoundingClientRect();
+    const tenantButtonBox = tenant.getBoundingClientRect();
+
+    const horizontalDistanceFromLandLord = distanceFromCenter(landLordButtonBox.x, x , landLordButtonBox.width);
+    const horizontalDistanceFromTenant = distanceFromCenter(tenantButtonBox.x, x , tenantButtonBox.width);
+
+    const verticalDistanceFromLandLord = distanceFromCenter(landLordButtonBox.y, y , landLordButtonBox.height);
+    const verticalDistanceFromTenant = distanceFromCenter(tenantButtonBox.y, y , tenantButtonBox.height);
+
+
+    const horizontalOffsetLandlord = landLordButtonBox.width / 2 + OFFSET
+    const verticalOffsetLandlord = landLordButtonBox.height / 2 + OFFSET
+
+    const horizontalOffsetTenant =tenantButtonBox.width / 2 + OFFSET
+    const verticalOffsetTenant = tenantButtonBox.height / 2 + OFFSET
+
+
+   
+    
+
+
+    if((Math.abs(horizontalDistanceFromLandLord) <= horizontalOffsetLandlord && Math.abs(verticalDistanceFromLandLord) <= verticalOffsetLandlord) ||  (Math.abs(horizontalDistanceFromTenant) <= horizontalOffsetTenant && Math.abs(verticalDistanceFromTenant) <= verticalOffsetTenant ) ){
+        if(Math.abs(horizontalDistanceFromLandLord) <= horizontalOffsetLandlord && Math.abs(verticalDistanceFromLandLord) <= verticalOffsetLandlord){
+            console.log('landlord')
+            // landLord.style.float="right"
+            landLord.style.visibility="hidden"
+               
+        }
+        else{
+            console.log('tenant')
+            // tenant.style.float="left"
+            tenant.style.visibility="hidden"
+        }
+    }else {
+        // landLord.style.float="left"
+        // tenant.style.float="right"
+        landLord.style.visibility="visible"
+        tenant.style.visibility="visible"
+    }
+
+}
+
+
+function validationInfo(){
+    var flag = 0;
+    if (password.value != rePassword.value){
+            return 1;
+    }
+    else if (password == '' ){
+        return 2;
+    }
+    else if (fName.value.length<3){
+        return 3;
+    }else if (lName.value.length<3){
+        return 4;
+    }
 }
