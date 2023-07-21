35b8cb0d6e462783e7bac7cfb519f6acb5cf7a37
Sandesh <khatiwadasandesh01@gmail.com>
2023-07-21 15:27:11 +0545

    Forgot Password Functionality Implemented

--- Public/emailVerification.php
+++ Public/emailVerification.php
@@ -1,3 +1,62 @@
+
+<?php
+
+require('./connect.php');
+$errMessage = "";
+
+
+
+if(isset($_POST['submit'])){
+
+
+    $enteredEmail = $_POST['email'];
+
+
+    $query1 = "SELECT * FROM landlord WHERE email = '$enteredEmail' ";
+    $query2 = "SELECT * FROM tenant WHERE email = '$enteredEmail'";
+
+    
+
+    $queryL = mysqli_query($connection,$query1);
+    $queryT = mysqli_query($connection,$query2);
+    $countLandlord = mysqli_num_rows($queryL);
+    $countTenant = mysqli_num_rows($queryT);
+
+
+
+
+    if($countLandlord==1 || $countTenant==1){
+
+
+
+
+            session_start();
+            if($countLandlord==1){
+                $array1=mysqli_fetch_assoc($queryL);
+                $_SESSION['l_id']= $array1['l_id'];
+                $_SESSION['email'] = $array1['email'];
+                $_SESSION['t_id'] = -1;
+    
+                
+            }
+            
+            if($countTenant==1){
+                $array2=mysqli_fetch_assoc($queryT);
+                $_SESSION['t_id']= $array2['t_id'];
+                $_SESSION['email'] = $array2['email'];
+                $_SESSION['l_id'] = -1;
+            }
+    }else{
+       $errMessage="Email not registered";
+    }
+
+
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,16 +69,66 @@
     <script src="./script/index.js" defer></script>
           <!-- This includes all the Navbar and relevant heading section-->
           <script src="./script/header.js"></script>
+
+            <!-- smtp js to send email for otp verification  -->
+        <script src="https://smtpjs.com/v3/smtp.js"></script>
+
+
 </head>
 <body>
         <div class="container hidden"><br>
             <h2 class="title-h2">Kotha Chaiyo</h2>
         <h3 class="title-h3">Ultimate Property Finder</h3><hr><br>
-            <form action="" method="post">
+            <form action="" method="POST">
                 <label for="container-emailV"><h2 class="container-h2">Enter your email:</h2></label>
-                <input type="email" name="" id="container-emailV" required><br><br>
+                <input type="email" name="email" id="container-emailV" required><p style="color:red;font-size:1.5vw;"><?php echo $errMessage; ?></p>
+            <button name="submit" type="submit" class="container-button">Next</button>
             </form>
-            <button class="container-button" onclick="window.location.href='emailVerification2.php'">Next</button>
+         
         </div>
 </body>
 </html>
+
+
+
+<?php
+
+if(isset($_POST["submit"])){
+    if($countLandlord==1 || $countTenant==1){ 
+
+        
+            $randomNumber = rand(00000, 99999); 
+            $_SESSION['otp'] = $randomNumber;
+           
+        ?>
+             <script>
+           
+           const email = "<?php echo $enteredEmail; ?>";
+   
+   
+   
+           const otp = <?php echo json_encode($randomNumber); ?>;  //generate random otp 
+   
+   
+        
+
+
+           Email.send({
+               Host : "smtp.elasticemail.com",
+               Username : "khatiwadasandesh01@gmail.com",
+               Password : "455BDBFAC2E8D27E7AA95EE7294E885CB01C",
+               To : email,
+               From : "khatiwadasandesh01@gmail.com",
+               Subject : "Welcome to Kotha Chaiyo",
+               Body : `Hey ! Your One Time Password is : ${otp}. Don't Share this password to anyone.`
+           }).then(()=>{
+            window.location.href="emailVerification2.php"
+           })
+             </script>
+        <?php
+
+    }
+}
+
+
+?>
