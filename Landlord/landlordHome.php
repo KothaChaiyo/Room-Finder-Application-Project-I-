8caf97de4687983bcec66e9a50aa4715bcbcd6ca
Sandesh <khatiwadasandesh01@gmail.com>
2023-07-21 10:57:09 +0545

    Login and Logout Functionality added

--- Landlord/landlordHome.php
+++ Landlord/landlordHome.php
@@ -1,5 +1,37 @@
+
 <?php
+session_start();
 
-echo "Landlord Homepage";
+if(!isset($_SESSION['lname']) || !isset($_SESSION['lemail']) || !isset($_SESSION['lcontact'])){
 
+    header("HTTP/1.0 404 Not Found");
+    echo "<h1>404 Not Found</h1>";
+    echo "The requested URL was not found on this server.";
+    exit;
+
+}
+
+echo "Landlord Homepage <br>";
+echo "Welcome!!! "; 
+echo $_SESSION['lname'];
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        
+    </style>
+</head>
+
+<body>
+    <a href="../Public/logout.php"target="_self">Logout</a>
+    
+</body>
+</html>
--- Tenant/tenantHome.php
+++ Tenant/tenantHome.php
@@ -1,5 +1,36 @@
 <?php
 
-echo "Tenant Homepage";
+session_start();
 
+if(!isset($_SESSION['tname']) || !isset($_SESSION['temail']) || !isset($_SESSION['tcontact'])){
+
+    header("HTTP/1.0 404 Not Found");
+    echo "<h1>404 Not Found</h1>";
+    echo "The requested URL was not found on this server.";
+    exit;
+
+}
+
+echo "Tenant Homepage <br>";
+echo "Welcome!!! "; 
+echo $_SESSION['tname'];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        
+    </style>
+</head>
+
+<body>
+    <a href="../Public/logout.php"target="_self">Logout</a>
+    
+</body>
+</html>
