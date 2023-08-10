28d131ca56faee45e6be02a8d71ed2c0a72b2060
Sandesh <khatiwadasandesh01@gmail.com>
2023-08-10 21:28:07 +0545

    Update staticMap.js

--- Landlord/staticMap.js
+++ Landlord/staticMap.js
@@ -1,30 +1,4 @@
-// async function writeCoordinates() {
-
-//     await fetch("coordinates.json", {
-//          method: "POST",
-//          headers: {
-//              'Content-Type': 'application/json'  
-//          },
-//          body: JSON.stringify({
-//              "latitude": latitude,
-//              "longitude": longitude
-//          })
-//      });
-     
-//      // const data = await response.json();
-     
-//      // console.log(data);  
- 
-//     }
-
-
-
-  
-  
-        
-  //   API Key : pk.eyJ1IjoidGhlLW1heS1ndXkiLCJhIjoiY2xoeXkyemxtMGJqbTNkcDV4cDRtYW5tNiJ9.ngKtQaQqRWtYdwNqpO3Tnw
-  
-      // Initialize the map
+   // Initialize the map
 
   
       let latitude , longitude;
@@ -47,13 +21,7 @@
          latitude = marker._lngLat.lat;
         
         console.log(longitude,latitude);
-  
-  
-  
-      //   writeCoordinates();
-  
-  
-  
+
       sendToServer(longitude,latitude);
   
   
@@ -61,37 +29,6 @@
       });
   
   
-  
-      // map.addEventListener("click",function setCoords(event){
-  
-      //     marker.remove();
-  
-      //     const marker = new mapboxgl.Marker({
-      //         color: "#FFFFFF",
-      //         draggable: true
-      //         }).setLngLat(event.lngLat)
-      //         .addTo(map);
-          
-          
-  
-      //    longitude = marker._lngLat.lng;
-      //    latitude = marker._lngLat.lat;
-        
-      //   console.log(longitude,latitude);
-  
-      //   writeCoordinates();
-          
-        
-      // });
-      
-      // // Add predefined markers
-      // const marker1 = new mapboxgl.Marker()
-      //   .setLngLat([85.300140, 27.700769])
-      //   .addTo(map);
-    
-  
-    
-      // ...
   
   
   
